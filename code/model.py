d9dcce86c8053c09619167deb3f2627005c435fa
Kayla Andersen <kaynandersen@gmail.com>
2016-09-03 13:27:03 -0600

    Made code directory with python starter file and eda notebook

---
+++ code/model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# input - move to command line eventually
+data_source = r'../data/Ibotta_Marketing_Sr_Analyst_Dataset.csv'
+
+# Read data
+df = pd.read_csv(data_source)
+
+# data cleaning
+
+# eda
